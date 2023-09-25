5b78de0bb9a284e869a78581619b7bc6c3f2d2f9
yleosamp <leonnardomelo3@gmail.com>
2023-09-24 22:56:05 -0300

    Init About Page

--- web/server.js
+++ web/server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.sendFile('/home/yleo/Documents/! PROJETOS/site_samples/web/index.html')
 })
 
+app.get('/sobre', (req, res) => {
+  res.sendFile('/home/yleo/Documents/! PROJETOS/site_samples/web/about.html')
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando!')
 })
